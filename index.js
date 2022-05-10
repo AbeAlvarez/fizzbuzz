ca7b41f842aaf94447376a9a9d947ac7c88b158b
AbeAlvarez <adv.blvd@gmail.com>
2022-05-10 12:46:48 -0500

    9 FizzbuzzService.js actualización

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const Reader = require ("./lib/utils/Reader")
+// Clase anterior con la que obtenemos los explorers
+const Reader = require("./lib/utils/Reader")
+const ExplorerService = require("./lib/services/ExplorerService")
+const FizzbuzzService = require ("./lib/services/FizzbuzzService")
+const explorers = Reader.readJsonFile("explorers.json")
 
-const explorers = Reader.readJsonFile("explorers.json");
 console.log(explorers)
+
+// Aplicación del ExplorerService sobre la lista de explorers
+console.log(ExplorerService.filterByMission(explorers, "node"))
+//ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+//ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+
+console.log(FizzbuzzService.applyValidationInExplorer(explorer1))
